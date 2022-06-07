e11af0ecff3671bf63336026a7200995134c7bb2
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-06-06 20:52:39 -0700

    tinitial files

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let id: number = 5; // number
+console.log('ID:', id);
+let company: string = 'On Demand Economy Labs'; // string
+let isPublished: boolean = true; // boolean
+let x: any = 'Hello'; // any type
