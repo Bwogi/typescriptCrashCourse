bea78dc1ce300ce770c11eabc98891a4e755abbe
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-06-07 21:22:14 -0700

    add more ts

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,20 @@ console.log('ID:', id);
 let company = 'On Demand Economy Labs';
 let isPublished = true;
 let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'hello'];
+let person = [1, 'Bwogi', true];
+let employee;
+employee = [
+    [1, 'Andrew'],
+    [2, 'Bwogi'],
+    [3, 'Ezera'],
+];
+var Direction1;
+(function (Direction1) {
+    Direction1["Up"] = "Up";
+    Direction1["Down"] = "Down";
+    Direction1["Left"] = "Left";
+    Direction1["Right"] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up);
